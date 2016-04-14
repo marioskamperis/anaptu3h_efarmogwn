ce0dc490dc44f39095e54d4aafd181b454843792
redangel1993 <Kamperis1993@hotmail.com>
2016-04-14 19:47:34 +0300

    Initial commit

---
+++ Config.php
@@ -0,0 +1,10 @@
+<?php
+ 
+/**
+ * Database config variables
+ */
+define("DB_HOST", "localhost:3306");
+define("DB_USER", "root");
+define("DB_PASSWORD", "");
+define("DB_DATABASE", "texnologia_polumeswn");
+?>
